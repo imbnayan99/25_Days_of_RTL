193aa8dceef311dfc3564ae8a7b5524d68725ead
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:26:30 +0530

    Update carry_select.v

--- Day 23 : 4-bit Carry Select Adder/carry_select.v
+++ Day 23 : 4-bit Carry Select Adder/carry_select.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:33:49 01/10/2023 
-// Design Name: 
-// Module Name:    carry_select 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module full_adder( A, B, Cin, S, Cout);
  input wire A, B, Cin;
  output reg S, Cout;
