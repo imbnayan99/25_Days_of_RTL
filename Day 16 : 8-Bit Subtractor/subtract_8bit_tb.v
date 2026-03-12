450989f25c42e38c0207d7eb09a7feafc1284cd5
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:23:49 +0530

    Update subtract_8bit_tb.v

--- Day 16 : 8-Bit Subtractor/subtract_8bit_tb.v
+++ Day 16 : 8-Bit Subtractor/subtract_8bit_tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    05:25:56 01/08/2023 
-// Design Name: 
-// Module Name:    subtract_8bit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module subtract_8bit (a, b, result);
 //define inputs and outputs
 input [7:0] a, b;
