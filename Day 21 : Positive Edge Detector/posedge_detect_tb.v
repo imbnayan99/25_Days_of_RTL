02c545ff5aeaef0095f102fdda04a5b2bcc7e755
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:25:51 +0530

    Update posedge_detect_tb.v

--- Day 21 : Positive Edge Detector/posedge_detect_tb.v
+++ Day 21 : Positive Edge Detector/posedge_detect_tb.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   17:55:04 01/12/2023
-// Design Name:   edge_detector
-// Module Name:   /home/ise/Desktop/edge_det/edge_detector_tb.v
-// Project Name:  edge_det
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: edge_detector
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module edge_detector_tb;
 
