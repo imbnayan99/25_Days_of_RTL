3483b4ec2fb8af125e919b28d96d6f45d145fcde
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:21:59 +0530

    Update pipo_reg.v

--- Day 12 : Parallel in Parallel Out Register/pipo_reg.v
+++ Day 12 : Parallel in Parallel Out Register/pipo_reg.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    08:54:05 01/04/2023 
-// Design Name: 
-// Module Name:    pipo 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module pipo_reg(clk,clear,d,q);
 
 input clk,clear;
@@ -32,9 +14,7 @@ if (clear)
 
 q=  4'b0000;
 
-
 else
-
 
 q=d;
 end
