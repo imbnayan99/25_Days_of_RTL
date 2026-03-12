7dab1e4e9d9fecb2b1c0e00880057ec9dee5abb5
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:24:22 +0530

    Update multiplier_4bit_tb.v

--- Day 18 : 4-bit Multiplier/multiplier_4bit_tb.v
+++ Day 18 : 4-bit Multiplier/multiplier_4bit_tb.v
@@ -1,26 +1,6 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   10:10:06 01/09/2023
-// Design Name:   multiplier_4bit
-// Module Name:   /home/ise/Desktop/multiplier/multiplier_4bit_tb.v
-// Project Name:  multiplier
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: multiplier_4bit
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
+
 
 module multiplier_4bit_tb();
 reg [3:0] a,b;
