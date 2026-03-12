c475559fbbdc1382d3f09f28e58653b9d9a7bed8
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:26:41 +0530

    Update carry_select_tb.v

--- Day 23 : 4-bit Carry Select Adder/carry_select_tb.v
+++ Day 23 : 4-bit Carry Select Adder/carry_select_tb.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   13:46:09 01/10/2023
-// Design Name:   carry_select
-// Module Name:   /home/ise/Desktop/carry_select/carry_select_tb.v
-// Project Name:  carry_select
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: carry_select
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module carry_select_tb();
 reg [3:0]x,y;
