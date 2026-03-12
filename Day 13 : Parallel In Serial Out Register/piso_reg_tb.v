075c18f817ad8bf6cd6ee1b5eafd18128ecf9a4b
Nayan Barua <111595011+imbnayan99@users.noreply.github.com>
2026-03-13 02:22:49 +0530

    Update piso_reg_tb.v

--- Day 13 : Parallel In Serial Out Register/piso_reg_tb.v
+++ Day 13 : Parallel In Serial Out Register/piso_reg_tb.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   17:15:38 01/04/2023
-// Design Name:   piso_reg
-// Module Name:   /home/ise/Desktop/piso/piso_reg_tb.v
-// Project Name:  piso
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: piso_reg
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module piso_reg_tb();
 reg [3:0]d;
